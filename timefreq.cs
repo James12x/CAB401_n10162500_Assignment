13cf79ccc3eb618ee5a7784d5ac6d8a3b6723e9b
James12x <xeroulax@gmail.com>
2018-10-29 01:07:08 +1000

    commit

---
+++ timefreq.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Numerics;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DigitalMusicAnalysis {
+    public class timefreq {
+        public float[][] timeFreqData;
+        public int wSamp;
+        public Complex[] twiddles;
+
+        public timefreq(float[] x, int windowSamp) {
+            int ii;
+            double pi = 3.14159265;
+            Complex i = Complex.ImaginaryOne;
+            this.wSamp = windowSamp;
+            twiddles = new Complex[wSamp];
+            for (ii = 0; ii < wSamp; ii++) {
+                double a = 2 * pi * ii / (double)wSamp;
+                twiddles[ii] = Complex.Pow(Complex.Exp(-i), (float)a);
+            }
+
+            timeFreqData = new float[wSamp / 2][];
+
+            int nearest = (int)Math.Ceiling((double)x.Length / (double)wSamp);
+            nearest = nearest * wSamp;
+            Complex[] compX = new Complex[nearest];
+            for (int kk = 0; kk < nearest; kk++) {
+                if (kk < x.Length) {
+                    compX[kk] = x[kk];
+                } else {
+                    compX[kk] = Complex.Zero;
+                }
+            }
+            
+            int cols = 2 * nearest / wSamp;
+            for (int jj = 0; jj < wSamp / 2; jj++) {
+                timeFreqData[jj] = new float[cols];
+            }
+            timeFreqData = stft(compX, wSamp);
+        }
+
+        float[][] stft(Complex[] x, int wSamp) {
+            
+            Stopwatch test = new Stopwatch();
+
+            int ii = 0;
+            int jj = 0;
+            int kk = 0;
+            int ll = 0;
+            int N = x.Length;
+
+            float fftMax = 0;
+            int loopCount = 2 * (int)Math.Floor((double)N / (double)wSamp) - 1;
+            
+            float[][] Y = new float[wSamp / 2][];
+            for (ll = 0; ll < wSamp / 2; ll++) {
+                Y[ll] = new float[2 * (int)Math.Floor((double)N / (double)wSamp)];
+
+            }
+
+      /*      void Worker(int p) {
+                int i = (int)p;
+                Complex[] temp2 = new Complex[wSamp];
+                Complex[] tempFFT2 = new Complex[wSamp];
+                for (int j = 0; j < wSamp; j++) {
+                    temp2[j] = x[i * (wSamp / 2) + j];
+                }
+
+                tempFFT2 = fft(temp2);
+                // wsamp = 2048
+                for (int k = 0; k < wSamp / 2; k++) {
+                    Y[k][i] = (float)Complex.Abs(tempFFT2[k]);
+
+                    if (Y[k][i] > fftMax) {
+                        fftMax = Y[k][i];
+                    }
+                }
+            }*/
+
+            void Worker2(int start, int end) {
+                Complex[] temp2 = new Complex[wSamp];
+                Complex[] tempFFT2 = new Complex[wSamp];
+
+                for (int i = start; i < end; i++) {
+                    for (int j = 0; j < wSamp; j++) {
+                        temp2[j] = x[i * (wSamp / 2) + j];
+                    }
+
+                    tempFFT2 = fft(temp2);
+                    
+                    for (int k = 0; k < wSamp / 2; k++) {
+                        Y[k][i] = (float)Complex.Abs(tempFFT2[k]);
+
+                        if (Y[k][i] > fftMax) {
+                            fftMax = Y[k][i];
+                        }
+                    }
+                }
+            }
+
+            MainWindow.DoWorkInParallel(Worker2, loopCount);
+            
+        
+
+            /*  var tasks = new List<Task>();
+              for (int p = 0; p < asd; p++) {
+                  int index = p;
+                  tasks.Add(Task.Run(() => Worker(index)));
+                  //  Worker();
+              }*/
+            
+            //  Task.WaitAll(tasks.ToArray());
+
+            /*  Parallel.For(0, loopCount ,i => {
+                  Complex[] temp2 = new Complex[wSamp];
+                  Complex[] tempFFT2 = new Complex[wSamp];
+                  for (int j = 0; j < wSamp; j++) {
+                        temp2[j] = x[i * (wSamp / 2) + j];
+                    }
+
+                    tempFFT2 = fft(temp2);
+                  
+                    for (int k = 0; k < wSamp / 2; k++) {
+                        Y[k][i] = (float)Complex.Abs(tempFFT2[k]);
+
+                        if (Y[k][i] > fftMax) {
+                          fftMax = Y[k][i];
+                      }
+                    }
+                });*/
+
+            
+            /*     Parallel.For(0, threadCount, p => {
+                     int end = amountPartition * (p + 1) + (p != 0 ? leftOver : 0); //offset the ending for the rest of the thread
+                     int start = end - amountPartition;
+                     end += (p == 0 ? leftOver : 0); //add leftOver to the first thread 'after' assigning declaring so that 'start' will have value of 0
+                     Complex[] temp2 = new Complex[wSamp];
+                     Complex[] tempFFT2 = new Complex[wSamp];
+
+                     for (int i = start; i < end; i++) {
+                         for (int j = 0; j < wSamp; j++) {
+                             temp2[j] = x[i * (wSamp / 2) + j];
+                         }
+
+                         tempFFT2 = fft(temp2);
+                         // wsamp = 2048
+                         for (int k = 0; k < wSamp / 2; k++) {
+                             Y[k][i] = (float)Complex.Abs(tempFFT2[k]);
+
+                             if (Y[k][i] > fftMax) {
+                                 fftMax = Y[k][i];
+                             }
+                         }
+                     }
+                 });*/
+
+         
+           
+               void Worker3(int start, int end) {
+                   for (int i = start; i < end; i++) {
+                       for (int k = 0; k < wSamp / 2; k++) {
+                           Y[k][i] /= fftMax;
+                       }
+                   }
+               }
+
+         
+            MainWindow.DoWorkInParallel(Worker3, loopCount);
+
+            /*  Parallel.For(0, threadCount, p => {
+                  int end = amountPartition * (p + 1) + (p != 0 ? leftOver : 0); //offset the ending for the rest of the thread
+                  int start = end - amountPartition;
+                  end += (p == 0 ? leftOver : 0); //add leftOver to the first thread 'after' assigning declaring so that 'start' will have value of 0
+                  for (int i = start; i < end; i++) {
+                      for (int k = 0; k < wSamp / 2; k++) {
+                          Y[k][i] /= fftMax;
+                      }
+                  }
+              });*/
+
+            return Y;
+        }
+
+      
+
+        Complex[] fft(Complex[] x) {
+            int ii = 0;
+            int kk = 0;
+            int N = x.Length;
+
+            Complex[] Y = new Complex[N];
+
+            // NEED TO MEMSET TO ZERO?
+
+            if (N == 1) {
+                Y[0] = x[0];
+            } else {
+
+                Complex[] E = new Complex[N / 2];
+                Complex[] O = new Complex[N / 2];
+                Complex[] even = new Complex[N / 2];
+                Complex[] odd = new Complex[N / 2];
+
+
+               
+                for (ii = 0; ii < N; ii++) {
+
+                    if (ii % 2 == 0) {
+                        even[ii / 2] = x[ii];
+                    }
+                    if (ii % 2 == 1) {
+                        odd[(ii - 1) / 2] = x[ii];
+                    }
+                }
+               
+
+
+                E = fft(even);
+                O = fft(odd);
+
+
+
+                for (kk = 0; kk < N; kk++) {
+                    Y[kk] = E[(kk % (N / 2))] + O[(kk % (N / 2))] * twiddles[kk * wSamp / N];
+                }
+            }
+
+            return Y;
+        }
+
+    }
+}
---
+++ wavefile.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace DigitalMusicAnalysis
+{
+    public class wavefile
+    {
+        public float[] wave;
+        public byte[] data;
+        public char[] ChunkID = new char[4];
+	    public int ChunkSize;
+	    public char[] Format = new char[4];
+	    public char[] Subchunk1ID = new char[4];
+	    public int Subchunk1Size;
+	    public short AudioFormat;
+	    public short NumChannels;
+	    public int SampleRate;
+	    public int ByteRate;
+	    public short BlockAlign;
+	    public short BitsPerSample;
+        
+	    public char[] Subchunk2ID = new char[4];
+	    public int Subchunk2Size;
+
+        public wavefile(FileStream file)
+        {
+            BinaryReader binRead = new BinaryReader(file);
+
+            ChunkID =  binRead.ReadChars(4);
+            ChunkSize = binRead.ReadInt32();
+            Format = binRead.ReadChars(4);
+            Subchunk1ID = binRead.ReadChars(4);
+            Subchunk1Size = binRead.ReadInt32();
+            AudioFormat = binRead.ReadInt16();
+            NumChannels = binRead.ReadInt16();
+            SampleRate = binRead.ReadInt32();
+            ByteRate = binRead.ReadInt32();
+            BlockAlign = binRead.ReadInt16();
+            BitsPerSample = binRead.ReadInt16();
+            
+            char testValue = 'd';
+            while (true) {
+                if (binRead.ReadChar() == testValue) {
+                    binRead.BaseStream.Seek(-1, SeekOrigin.Current);
+                    Subchunk2ID = binRead.ReadChars(4);
+                    break;
+                }
+            }
+            
+            Subchunk2Size = binRead.ReadInt32();
+
+            int numSamples = Subchunk2Size / (BitsPerSample / 8);
+            
+            data = new byte[numSamples];
+            wave = new float[numSamples];
+            
+            data = binRead.ReadBytes(numSamples);
+        /*    MessageBox.Show("ChunkID " + new string(ChunkID) + "\n" +
+                            "ChunkSize " + ChunkSize + "\n" +
+                            "Format " + new string(Format) + "\n" +
+                            "Subchunk1ID " + new string(Subchunk1ID) + "\n" +
+                            "Subchunk1Size " + Subchunk1Size + "\n" +
+                            "AudioFormat " + AudioFormat + "\n" +
+                            "NumChannels " + NumChannels + "\n" +
+                            "SampleRate " + SampleRate + "\n" +
+                            "ByteRate " + ByteRate + "\n" +
+                            "BlockAlign " + BlockAlign + "\n" +
+                            "BitsPerSample " + BitsPerSample + "\n" +
+                            "Subchunk2ID " + new string(Subchunk2ID).ToString() + "\n" +
+                            "Subchunk2Size " + Subchunk2Size + "\n" +
+                            "numSamples " + numSamples + "\n" +
+                            "data " + data);*/
+
+            for (int i = 0; i < numSamples; i++)
+            {
+                wave[i] = ((float)data[i] - 128) / 128;
+            }
+
+        }
+    }
+}
